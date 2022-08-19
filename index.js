26458b057c6042657f97dfe9e180b1007b7400ae
Sunethia <lawrencesunethia@gmail.com>
2022-08-19 11:15:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express"); // Used to set up a server
+const cors = require("cors"); // Used to prevent errors when working locally
+const path = require("path");
+
+const app = express(); // Initialize express as an app variable
+app.set("port", process.env.PORT || 6969); // Set the port
+app.use(express.json()); // Enable the server to handle JSON requests
+app.use(cors()); // Dont let local development give errors
+
+const staticPath = path.join(__dirname + "public");
+// connecting my index.html
+app.use(express.static(staticPath));
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome" });
+});
+
+const userRoute = require("./routes/userRoute");
+app.use("/users", userRoute);
+
+const productsRoute = require("./routes/productRoute");
+app.use("/products", productsRoute);
+
+app.use(express.static("public"));
+app.get("products", function (req, res) {
+  res.sendFile(_dirname + "/" + "product.html");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`htps://localhost:${app.get("port")}`);
+  console.log("Press Ctrl+C to exit server");
+});
